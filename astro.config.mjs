31cbf0c74d1c9d2d670ee20b7052844adcdd4733
josue <josuezd00@gmail.com>
2024-03-22 15:11:44 -0500

    set config por github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: "https://slorg4.github.io",
+  base: "/coffe-list-challenge",
 });
